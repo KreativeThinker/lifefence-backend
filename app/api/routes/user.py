7000c534904ca72579689af1797839aa968ec8bb
KreativeThinker <sagely2005eagle@gmail.com>
2024-11-01 13:58:26 +0530

    chore: add route

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.api.routes.auth import get_current_user
+from app.models.user import User, User_Pydantic
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.get("/")
-async def get_user_data():
-    return "user"
+@router.get("/me", response_model=User_Pydantic)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return await User_Pydantic.from_tortoise_orm(current_user)
